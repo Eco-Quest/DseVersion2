d49635cdbf343f74b6be6585fc111c78a5315512
Matt MA <66322765+Eco-Quest@users.noreply.github.com>
2026-05-26 11:49:32 +0800

    initial

---
+++ DSEFinalVersion/DSEFinalVersion/DSEFinalVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSEFinalVersionApp.swift
+//  DSEFinalVersion
+//
+//  Created by Matt on 2026/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DSEFinalVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
